1f85a19d46165baa6dc269ae255a7ed7360f9034
deeservent <onion.dzw@icloud.com>
2022-04-21 23:55:06 +0800

    代码初始化

---
+++ nightingale-assistant.py
@@ -0,0 +1,126 @@
+import logging
+import sys
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings, QWebEngineProfile
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+class MainWindow(QMainWindow):
+
+    baseUrl = "https://wan.ludashi.com/"
+    loginUrl = "https://wan.ludashi.com/account/index"
+    gamePage = "http://wan.ludashi.com/yeyou/cjzg"
+    serverArea = 789
+    loginStatus = False
+    ckCode = False
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        logging.info(args)
+        self.setWindowTitle('夜莺辅助')
+        self.setWindowIcon(QIcon("extend/images/title.jpeg"))
+        self.showMaximized()
+
+        # 设置浏览器
+        self.browser = QWebEngineView()
+        self.browser.page().profile().cookieStore().deleteAllCookies()
+        self.browser.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+        self.browser.settings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
+        self.browser.settings().setAttribute(QWebEngineSettings.FullScreenSupportEnabled, True)
+
+
+        # 添加浏览器到窗口中
+        self.setCentralWidget(self.browser)
+        # 指定打开界面的 URL
+        self.browser.setUrl(QUrl(self.loginUrl))
+        self.browser.page().loadFinished.connect(self.autoLogin)
+        # 让浏览器相应url地址的变化
+        self.browser.urlChanged.connect(self.opendGame)
+
+
+    def autoLogin(self):
+        if self.loginStatus == 1 :
+            return
+        userName = "13437176465"
+        passWord = "Ding1990"
+        # userName = "smpyxy01"
+        # passWord = "123123"
+        js_function = '''
+                function autoLogin()
+                {{
+                    var usernameInput = document.getElementsByName("username")[0];
+                    var passwordInput = document.getElementsByName("password")[0];
+                    var loginSubmit = document.getElementById("loginSubmit");
+                    if(loginSubmit == undefined){{
+                      return "faild"
+                    }}
+                    else{{
+                      usernameInput.value="{un}";
+                      passwordInput.value="{pwd}";
+                      loginSubmit.click();
+                      return "success"
+                    }}
+                }}
+                autoLogin();
+                '''.format(un=userName, pwd=passWord)
+        self.browser.page().runJavaScript(js_function, self.login_callback)
+
+    def login_callback(self, result):
+        if result == 'success':
+            print("登录成功!")
+            self.loginStatus = True
+            self.checkCaptchaCode()
+        else:
+            print("登录失败！", result)
+
+    def checkCaptchaCode(self):
+        if self.ckCode == 1 :
+            return
+        js_function = '''
+            function getCaptchaCode()
+            {
+              var captchaCode = document.getElementsByClassName('captcha_img')[0]
+              if(captchaCode.children[0] == undefined){{
+                  return "faild"
+              }}else{{
+                  return captchaCode.children[0].src
+              }}
+            }
+            getCaptchaCode();
+            '''
+        self.browser.page().runJavaScript(js_function, self.ckCaptchaCode_callback)
+
+    def ckCaptchaCode_callback(self, result):
+        print("获取验证码", result)
+        if result == "faild" :
+            self.checkCaptchaCode()
+        else:
+            return
+
+    def opendGame(self, qurl):
+        # 将当前网页的链接更新到地址栏
+        urlStr = qurl.toString()
+        print(urlStr)
+        if urlStr == self.baseUrl:
+            self.ckCode = True
+            gameUrl = "{0}?s={1}".format(self.gamePage, self.serverArea)
+            print("进入游戏页面")
+            self.browser.page().load(QUrl(gameUrl))
+        elif urlStr.find(self.gamePage) >= 0:
+            print("play game")
+
+
+
+if __name__ == "__main__":
+    argvs = sys.argv
+    # 创建应用
+    app = QApplication(sys.argv)
+    # 创建主窗口
+    window = MainWindow()
+    # 显示窗口
+    window.show()
+    # 运行应用，并监听事件
+    app.exec_()
+    sys.exit(app.exec_())
---
+++ nightingale.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import json
+
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QUrl, QFileInfo
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QMainWindow
+
+from NightingaleGame import *
+
+
+class MainWindow(QMainWindow):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.setWindowTitle('夜莺辅助')
+    self.setWindowIcon(QIcon("extend/images/title.jpeg"))
+    self.browser_init()
+
+  def browser_init(self):
+      self.webview = WebEngineView(self)
+      self.webview.load(QUrl(QFileInfo("extend/start.html").absoluteFilePath()))
+      self.saveconfBtn = QPushButton("保存配置")
+      self.saveconfBtn.clicked.connect(self.saveconf)
+      self.playGameBtn = QPushButton("启动游戏")
+      self.playGameBtn.clicked.connect(self.startSelectGame)
+      main_frame = QWidget()
+      layout = QVBoxLayout(main_frame)
+      layout.addWidget(self.saveconfBtn)
+      layout.addWidget(self.playGameBtn)
+      layout.addWidget(self.webview)
+      self.setCentralWidget(main_frame)
+
+
+
+  def saveconf(self):
+      value = "saveconf"
+      self.webview.page().runJavaScript('saveconf("' + value + '");',self.saveconf_callback)
+
+  def saveconf_callback(self, result):
+      jsonStr = json.dumps(result,ensure_ascii=False)
+      print(jsonStr)
+      with open('extend/data/conf.json', 'w') as conf:
+          json.dump(result, conf)
+
+  def startSelectGame(self):
+      value = "startSelectGame"
+      self.webview.page().runJavaScript('getSelectRows("' + value + '");',self.startSelectGame_callback)
+
+  def startSelectGame_callback(self, result):
+      if result is None:
+          print("请选择账号")
+      else:
+          for acount in result:
+            username = acount["accout"]
+            password = acount["password"]
+            print(username,password)
+            self.gameweb = self.webview.createWindow(self)
+            ng = NightingaleGame(self.gameweb)
+
+
+
+class WebEngineView(QWebEngineView):
+    windowList = []
+    def createWindow(self, QWebEnginePage_WebWindowType):
+        new_webview = WebEngineView()
+        new_webview.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+        new_webview.settings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
+        new_webview.settings().setAttribute(QWebEngineSettings.FullScreenSupportEnabled, True)
+        new_window = MainWindow()
+        new_window.setCentralWidget(new_webview)
+        new_window.showMaximized()
+        self.windowList.append(new_window)  # 注：没有这句会崩溃！！！
+        return new_webview
+
+if __name__ == "__main__":
+    argvs = sys.argv
+    app = QApplication(argvs)
+    mainWin = MainWindow()
+    mainWin.showMaximized()
+    sys.exit(app.exec_())
